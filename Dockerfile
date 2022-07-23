7c2bd86763c601229cb99fe6ab5c11f27090465b
Mikhail <mmikee00800@gmail.com>
2022-07-23 03:51:20 +0300

    video note transcription

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python3.10.5-slim
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install -r /requirements.txt
+COPY . .
+CMD ["python3", "main.py"]
+
+
+
--- handlers.py
+++ handlers.py
@@ -2,7 +2,7 @@ import logging
 import time
 from aiogram import Bot, Dispatcher, types, exceptions, executor
 from configparser import ConfigParser
-from core import AUDIO_ROOT, recognize_speech, mp3_to_vaw, ogg_to_vaw
+from core import FILE_ROOT, recognize_input
 import random
 import os
 
@@ -21,39 +21,31 @@ async def send_welcome(message: types.Message):
         await message.reply(file.read(), parse_mode=types.ParseMode.HTML)
 
 
-@dp.message_handler(content_types=types.ContentType.VOICE)
-async def voice_download(ms: types.Message):
-    file_id = ms.voice.file_id
-    file = await bot.get_file(file_id)
-    file_path = file.file_path
-    cache_name = f"{AUDIO_ROOT}/{random.randint(1,1000)}"
-    ext = '.ogg'
-    cache_path = f"{cache_name}{ext}"
-    print('voice recieved', cache_path)
-    await bot.download_file(file_path, cache_path)
-    if os.path.exists(cache_path):
-        ogg_to_vaw(cache_path)
-        text = recognize_speech(f"{cache_name}.wav")
-        os.remove(f"{cache_name}.wav")
+@dp.message_handler(content_types=[types.ContentType.VOICE,
+                                   types.ContentType.AUDIO,
+                                   types.ContentType.VIDEO_NOTE])
+async def voice_transcribe(ms: types.Message):
+    text = "cannot extract file from message"
+    if ms.voice:
+        file_id = ms.voice.file_id
+    elif ms.audio:
+        file_id = ms.audio.file_id
+    elif ms.video_note:
+        file_id = ms.video_note.file_id
     else:
-        raise OSError("path not found or file is not downloaded yet")
-    await bot.send_message(ms.chat.id, text)
-
-
-@dp.message_handler(content_types=types.ContentType.AUDIO)
-async def audio_download(ms: types.Message):
-    file_id = ms.audio.file_id
+        await bot.send_message(ms.chat.id, text)
     file = await bot.get_file(file_id)
     file_path = file.file_path
-    cache_name = f"{AUDIO_ROOT}/{random.randint(1,1000)}"
-    ext = '.mp3'
+    _, ext = os.path.splitext(file_path)
+    if ext == ".oga":
+        ext = ".ogg"
+    cache_name = f"{FILE_ROOT}/{random.randint(1,1000)}"
     cache_path = f"{cache_name}{ext}"
-    print('voice recieved', cache_path)
     await bot.download_file(file_path, cache_path)
     if os.path.exists(cache_path):
-        mp3_to_vaw(cache_path)
-        text = recognize_speech(f"{cache_name}.wav")
-        os.remove(f"{cache_name}.wav")
+        print('message recieved')
+        text = recognize_input(cache_path)
+        os.remove(cache_path)
     else:
         raise OSError("path not found or file is not downloaded yet")
     await bot.send_message(ms.chat.id, text)
@@ -63,6 +55,3 @@ async def audio_download(ms: types.Message):
 async def print_help_info(message: types.Message):
     with open('templates/help.html', 'r') as file:
         await message.reply(file.read())
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
