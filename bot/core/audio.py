13f875a9fac8f42b59670f55f4c5fcb8343ba2f9
Mikhail <mmikee00800@gmail.com>
2022-09-17 10:01:26 +0300

    fix

--- bot/core/audio.py
+++ bot/core/audio.py
@@ -1,7 +1,11 @@
 from .base import AudioBase
 from aiogram import types, Bot
+from logging import getLogger
 import datetime
 import os
+
+
+logger = getLogger(__name__)
 
 
 class VideoNote(AudioBase):
@@ -9,6 +13,7 @@ class VideoNote(AudioBase):
         self.message = ms
         self.file_id = ms.video_note.file_id
         self.bot = bot
+        self.ext = ".mp4"
 
     async def get_file(self):
         file = await self.bot.get_file(self.file_id)
@@ -25,6 +30,7 @@ class Audio(AudioBase):
         self.message = ms
         self.file_id = ms.audio.file_id
         self.bot = bot
+        self.ext = ".mp3"
 
     async def get_file(self):
         file = await self.bot.get_file(self.file_id)
@@ -40,13 +46,17 @@ class Voice(AudioBase):
         self.message = ms
         self.file_id = ms.voice.file_id
         self.bot = bot
+        self.ext = ".ogg"
 
     async def get_file(self):
         file = await self.bot.get_file(self.file_id)
         file_path = file.file_path
         _, ext = os.path.splitext(file_path)
-        
-        cache_name = f"cache/{}"
+
+        cache_path = f"cache/voice_{datetime.datetime.utcnow()}.ogg"
+        await self.bot.download_file(file_path, cache_path)
+        if os.path.exists(cache_path):
+            logg
 
     async def recognize_speech(self):
         pass
