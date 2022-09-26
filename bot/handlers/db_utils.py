9abb5b306e4ddd15fe1bbb43da6a7b26500a5d29
Mikhail <mmikee00800@gmail.com>
2022-09-26 21:41:42 +0300

    Update db_utils.py

--- bot/handlers/db_utils.py
+++ bot/handlers/db_utils.py
@@ -10,20 +10,21 @@ def get_session():
     return Session(bind=engine, autocommit=True)
 
 
-async def create_user_if_not_exists(session: Session, tg_id: int) -> None:
+async def create_user_if_not_exists(session: Session, tg_id: int) -> int:
     res = session.query(User).filter(User.chat_id == tg_id)
     user = User(chat_id=tg_id)
     if not res.one_or_none():
         session.add(user)
     else:
-        res.update(dict(chat_id=tg_id))
+        res.update(dict(chat_id=tg_id, last_use=datetime.datetime.utcnow()))
     session.flush()
+    return user.id
 
 
 async def post_action(session: Session, user_tg_id: int, action: str) -> None:
-    user = session.query(User).filter(User.chat_id == user_tg_id)
-    user.update(dict(chat_id=user_tg_id))
-    db_action = ActionInfo(user_id=user.one().id, user_action=action)
+    user_id = await create_user_if_not_exists(session, user_tg_id)
+    user.update(dict(chat_id=user_tg_id, last_use=datetime.datetime.utcnow()))
+    db_action = ActionInfo(user_id=user_id, user_action=action, date=datetime.datetime.utcnow())
     session.add(db_action)
     session.flush()
 
